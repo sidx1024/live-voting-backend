b2192e652dd551cb5980eda7ac4de96a80f94b5e
sidx1024 <vistax86live@gmail.com>
2018-04-01 16:50:38 +0530

    Update

--- index.js
+++ index.js
@@ -2,16 +2,13 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const {verifyIdToken} = require('./api');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(function (req, res, next) {
-  // TODO: Use safe origins only
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
-  next();
-});
+
+app.use(cors());
 
 app.post('/api/token/verify', verifyIdToken);
 
