9fc14a706342bcca1d575f391d2466519580bdea
sidx1024 <vistax86live@gmail.com>
2018-04-02 00:30:24 +0530

    Update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 
 const { verifyIdToken, jwtMiddleware } = require('./api');
 const { activateInterval } = require('./vote');
@@ -9,24 +10,27 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 80;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.post('/api/token/verify', verifyIdToken);
 
 app.get('/protected', jwtMiddleware, (req, res) => {
   res.send({ message: 'You accessed a protected endpoint successfully.' });
 });
-
+let users = 0;
 io.on('connection', (socket) => {
-  console.log('[+] a user connected');
-  console.log('[+] socket', socket);
+  users += 1;
+  console.log('[+] a user connected', users);
   socket.on('disconnect', () => {
-    console.log('[-] user disconnected');
+    users -= 1;
+    console.log('[-] user disconnected', users);
   });
 });
 
