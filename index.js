055f600511d5c3f73efdff01c67ff55a176b5988
sidx1024 <vistax86live@gmail.com>
2018-04-01 15:33:36 +0530

    Update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 const bodyParser = require('body-parser');
-const {verifyIdToken} = require('api');
+const {verifyIdToken} = require('./api');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
