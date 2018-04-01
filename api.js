4a6c7f14ca995d5ff306d6840da20edaa689956d
sidx1024 <vistax86live@gmail.com>
2018-04-01 15:31:58 +0530

    Update

---
+++ api.js
@@ -0,0 +1,40 @@
+const {OAuth2Client} = require('google-auth-library');
+
+function verifyIdToken(req, res) {
+  const CLIENT_ID = "881189081299-vlt4opk8p9ptobgivq84r6fbr63mebgs.apps.googleusercontent.com";
+  const client = new OAuth2Client(CLIENT_ID);
+
+  verify().catch((err) => {
+    res.status(401).json({
+      status: "Failed",
+      message: "Token cannot be verified."
+    });
+  });
+
+  async function verify() {
+    const { idToken } = req.body;
+
+    if(!idToken) {
+      res.json({
+        status: "Failed",
+        message: "Id token is missing"
+      });
+    }
+
+    const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: CLIENT_ID
+    });
+    const payload = ticket.getPayload();
+    const userid = payload['sub'];
+
+    const response = {
+      status: "Success",
+      message: "Token successfully verified."
+    };
+
+    res.json(response);
+  }
+}
+
+export {verifyIdToken};
--- index.js
+++ index.js
@@ -2,19 +2,17 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 const bodyParser = require('body-parser');
+const {verifyIdToken} = require('api');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(function(req, res, next) {
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(function (req, res, next) {
   // TODO: Use safe origins only
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
   next();
 });
 
-app.post('/api/token/verify', function(req, res) {
-  console.log('Request', req);
-  res.send('LOL!');
-});
+app.post('/api/token/verify', verifyIdToken);
 
 app.listen(port);
