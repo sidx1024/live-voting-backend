6a47f1ec3ebd5450b9c9b1bb493c7cb8a98979ab
sidx1024 <vistax86live@gmail.com>
2018-04-01 17:43:01 +0530

    Update

--- api.js
+++ api.js
@@ -1,10 +1,10 @@
 const {OAuth2Client} = require('google-auth-library');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 function verifyIdToken(req, res) {
   const CLIENT_ID = "881189081299-vlt4opk8p9ptobgivq84r6fbr63mebgs.apps.googleusercontent.com";
   const client = new OAuth2Client(CLIENT_ID);
-  console.info("-----------------------**---------------------------------------------");
-  console.info(req);
   verify().catch((err) => {
     res.status(401).json({
       status: "Failed",
@@ -15,13 +15,10 @@ function verifyIdToken(req, res) {
 
   async function verify() {
     const { idToken } = req.body;
-    console.info("----------------------------------------------------------------------");
-    console.info(req.body);
     if(!idToken) {
       return res.json({
         status: "Failed",
         message: "Id token is missing",
-        body: JSON.stringify(req.body)
       });
     }
 
@@ -29,16 +26,32 @@ function verifyIdToken(req, res) {
       idToken: idToken,
       audience: CLIENT_ID
     });
+
     const payload = ticket.getPayload();
     const userid = payload['sub'];
 
     const response = {
       status: "Success",
-      message: "Token successfully verified."
+      message: "Token successfully verified.",
+      userid: userid,
+      payload: payload
     };
-
-    return res.json(response);
+    
+    createToken({a: 255, b: 64}, function (token) {
+      res.json(Object.assign(response, {token}));
+    });
   }
+}
+
+function createToken(payload, callback) {
+  const SIGN_SECRET = process.env.SIGN_SECRET;
+  jwt.sign(payload, SIGN_SECRET, function (err, token) {
+    if(err) {
+      console.error('[createToken]', err);
+      return;
+    }
+    callback(token);
+  });
 }
 
 module.exports = {verifyIdToken};
