4e96802dab606cd2c3a42a9086c432bd6189e0b3
sidx1024 <vistax86live@gmail.com>
2018-04-01 19:01:19 +0530

    Update

--- api.js
+++ api.js
@@ -1,20 +1,25 @@
-const {OAuth2Client} = require('google-auth-library');
+const { OAuth2Client } = require('google-auth-library');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-function verifyIdToken(req, res) {
-  const CLIENT_ID = '881189081299-vlt4opk8p9ptobgivq84r6fbr63mebgs.apps.googleusercontent.com';
-  const client = new OAuth2Client(CLIENT_ID);
-  verify().catch((err) => {
-    res.status(401).json({
-      status: 'Failed',
-      message: 'Token cannot be verified.',
-      error: err.toString()
-    });
+const { SIGN_SECRET } = process.env;
+
+function createToken(payload, callback) {
+  jwt.sign(payload, SIGN_SECRET, (err, token) => {
+    if (err) {
+      console.error('[createToken]', err);
+      return;
+    }
+    callback(token);
   });
+}
+
+function verifyIdToken(req, res) {
+  const GOOGLE_CLIENT_ID = '881189081299-vlt4opk8p9ptobgivq84r6fbr63mebgs.apps.googleusercontent.com';
+  const client = new OAuth2Client(GOOGLE_CLIENT_ID);
 
   async function verify() {
-    const {idToken} = req.body;
+    const { idToken } = req.body;
 
     if (!idToken) {
       return res.json({
@@ -24,13 +29,15 @@ function verifyIdToken(req, res) {
     }
 
     const ticket = await client.verifyIdToken({
-      idToken: idToken,
-      audience: CLIENT_ID
+      idToken,
+      audience: GOOGLE_CLIENT_ID
     });
 
     const payload = ticket.getPayload();
-    const {email, name, picture, sub} = payload;
-    const data = {email, name, picture};
+    const {
+      email, name, picture, sub
+    } = payload;
+    const data = { email, name, picture };
 
     const response = {
       status: 'Success',
@@ -38,21 +45,32 @@ function verifyIdToken(req, res) {
       data
     };
 
-    createToken({...data, ...sub}, function (token) {
-      res.json({...response, token});
+    return createToken({ ...data, ...sub }, (token) => {
+      res.json({ ...response, token });
     });
   }
-}
 
-function createToken(payload, callback) {
-  const SIGN_SECRET = process.env.SIGN_SECRET;
-  jwt.sign(payload, SIGN_SECRET, function (err, token) {
-    if (err) {
-      console.error('[createToken]', err);
-      return;
-    }
-    callback(token);
+  verify().catch((err) => {
+    res.status(401).json({
+      status: 'Failed',
+      message: 'Token cannot be verified.',
+      error: err.toString()
+    });
   });
 }
 
-module.exports = {verifyIdToken};
+function jwtMiddleware(req, res, next) {
+  const { authorization } = req.headers;
+  if (!authorization) {
+    return res.status(403).json({ error: 'No credentials sent!' });
+  }
+  const token = authorization.split(' ')[1];
+  return jwt.verify(token, SIGN_SECRET, (err) => {
+    if (err) {
+      return res.status(403).json({ error: 'Error verifying credentials.' });
+    }
+    return next();
+  });
+}
+
+module.exports = { verifyIdToken, jwtMiddleware };
--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require('express');
+
 const app = express();
 const port = process.env.PORT || 8080;
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const {verifyIdToken} = require('./api');
+const { verifyIdToken, jwtMiddleware } = require('./api');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 
 app.post('/api/token/verify', verifyIdToken);
+
+app.get('/protected', jwtMiddleware, (req, res) => {
+  res.send({ message: 'You accessed a protected endpoint successfully.' });
+});
 
 app.listen(port);
